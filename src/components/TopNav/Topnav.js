5d3e06923d23f666d22aabd9a81dbb8c6908353e
junsyun <junsyun@junsyunui-MacBookPro.local>
2022-06-12 23:17:36 +0900

    doing : 🚧TopNav

---
+++ src/components/TopNav/Topnav.js
@@ -0,0 +1,21 @@
+import Signin from "./Signin/Signin.js"
+import Signup from "./Signup/Signup.js"
+import "antd/dist/antd.css"
+import { Button } from "antd"
+
+const TopNav = () => {
+	return (
+		<>
+		<nav className="top__bar" style={{  zIndex: 5, width: '100%', minHeight: '25px'}}>
+			<div className="menu__container">
+			<div className="menu_right">
+				<Button type="text">Sign in</Button>
+			</div>
+			</div>
+		</nav>
+
+		</>
+	)
+}
+
+export default Topnav
