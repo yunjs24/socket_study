4d7f7fb9aa2064c9f4e2b470e867e868019e455d
yunjs24 <yunjs24@gmail.com>
2022-06-24 18:19:12 +0900

    todo : file upload func

---
+++ frontend/src/utils/FileUpload.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import Dropzone from 'react-dropzone';
+import { Icon } from 'antd';
+import Axios from 'axios';
+function FileUpload(props) {
+
+	const [Images, setImages] = useState([])
+
+	return (
+		<>
+		</>
+	)
+}
+
+export default FileUpload
